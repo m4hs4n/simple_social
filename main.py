498120b38b7f2542b74489ec282f948f1056c726
m4hs4n <m4hs4nnn@gmail.com>
2026-05-12 13:26:24 +0200

    fix port render deployment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    uvicorn.run(app="app.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app="app.app:app", host="0.0.0.0",  port=int(os.environ.get("PORT", 8000)), reload=False)
